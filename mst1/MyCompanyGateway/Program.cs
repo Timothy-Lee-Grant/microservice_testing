646d81794d5624d180ae64e038b8a7301fa8ce83
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-06-13 16:17:52 -0700

    basic yarp project

--- mst1/MyCompanyGateway/Program.cs
+++ mst1/MyCompanyGateway/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Tell the service container to load and prepare the YARP engine, 
+// using the "ReverseProxy" section from our appsettings.json file above.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// 2. Inject YARP directly into the active HTTP request handling pipeline.
+app.MapReverseProxy();
 
 app.Run();
